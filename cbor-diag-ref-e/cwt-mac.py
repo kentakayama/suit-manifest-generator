549009926463619b44ebc0a1f128654d99d3ca49
Ken Takayama <ken.takayama.ietf@gmail.com>
2024-09-02 02:34:44 +0000

    fix: SUIT_Digest calculation on suit-manifest

--- cbor-diag-ref-e/cwt-mac.py
+++ cbor-diag-ref-e/cwt-mac.py
@@ -4,27 +4,30 @@ import sys
 from cwt import COSE, COSEKey, COSEKeyParams, COSEMessage
 
 if len(sys.argv) != 3:
-    sys.exit(-1)
+    print(f"{sys.argv[0]} [input cbor file] [output cose file]", file=sys.stderr)
+    sys.exit(1)
 
-with open(sys.argv[1], "r") as f:
-    input_hex = f.read()
+with open(sys.argv[1], "rb") as f:
+    input_bin = f.read()
 
 mac_key = COSEKey.new({
     COSEKeyParams.KTY: 4, # oct
     COSEKeyParams.ALG: 5, # HMAC 256/256
-    COSEKeyParams.K: bytes.fromhex("61" * 16) # aa...a (16)
+    COSEKeyParams.K: b'a' * 32 # '6161...61'
 })
 
 sender = COSE.new()
 encoded = sender.encode(
-    bytes.fromhex(input_hex),
+    input_bin,
     mac_key,
     protected={"alg": "HMAC 256/256"}
 )
 cose_mac0 = COSEMessage.loads(encoded)
 encoded, detached_payload = cose_mac0.detach_payload()
 
+# Verify that the library has produced valid COSE_Mac0 binary
+recipient = COSE.new()
+assert input_bin == recipient.decode(encoded.dumps(), mac_key, detached_payload=detached_payload)
 
 with open(sys.argv[2], "wb") as f:
     f.write(encoded.dumps())
-
